9da586e13130754b73846858d78839f723cb03cb
Johannes <h-annie@live.co.za>
2022-01-15 11:31:08 +0200

    Add some comments

--- lending.js
+++ lending.js
@@ -1,6 +1,11 @@
 const { retrieveLendingReserveInfo } = require("./pool_data");
 const { LENDING_RESERVES } = require("./solfarm_lending_reserves");
 
+/*
+Tracks the lending reserve info for all currencies from https://gist.github.com/therealssj/934c9b1d23a97f0d099b74abbdc31526
+Use 'node lending' to run 
+*/
+
 async function main() {
   console.log("Starting lending call");
   const reserveInfo = [];
